7e26a6a7209a611153ee733b4178144234599dd0
kris <krisbredemeier@gmail.com>
2017-09-22 18:06:57 -0700

    starting to work with react

--- 0x00-intro/8-react/src/index.js
+++ 0x00-intro/8-react/src/index.js
@@ -1,24 +1,3 @@
-// const $ = require('jquery')
-// import $ from "jquery";
-//
-// $(window).on('load', function() {
-//   $("body").after( $("<h1 id=\'header1\'>HolbertonSchool</h1>").hide());
-//   $("body").after( $("<h1 id=\'header2\'>HBTN</h1>").hide());
-//
-//   function show_second_h1() {
-//     $("#header1").hide();
-//     $("#header2").show();
-//     setTimeout(show_first_h1,2000);
-//   }
-//
-//   function show_first_h1() {
-//     $("#header1").show();
-//     $("#header2").hide();
-//     setTimeout(show_second_h1,2000);
-//   }
-//   setTimeout(show_second_h1,2000);
-// });
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';

37f7c43fd903e399c846fa359ac82d72f8b2b874
kris <krisbredemeier@gmail.com>
2017-09-24 20:07:39 -0700

    props to you

--- 0x00-intro/10-react_component_prop/src/components/title.jsx
+++ 0x00-intro/10-react_component_prop/src/components/title.jsx
@@ -4,12 +4,11 @@ import PropTypes from 'prop-types';
 export default class Title extends React.Component {
   render() {
     return (
-      <h1>Welcome to {this.props.name}</h1>
+      <h1>Welcome to Holberton School {this.props.name}</h1>
     );
   }
 }
 
 Title.propTypes = {
   name: PropTypes.string
-  name="Holberton School"
 };

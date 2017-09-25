bdbced28a6c04072a1dc62e090a2b0d1d92ae55a
kris <krisbredemeier@gmail.com>
2017-09-24 20:17:42 -0700

    advanced question

---
+++ 0x00-intro/100-react_prototype/src/components/title.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class Title extends React.Component {
+  render() {
+    return (
+      <h1>Holberton School {this.props.name}</h1>
+    );
+  }
+}
+
+Title.propTypes = {
+  name: PropTypes.string
+};

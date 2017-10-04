a081b90f2aca316d92f41308466062036620a6d4
kris <krisbredemeier@gmail.com>
2017-10-03 17:30:35 -0700

    creates class counter

--- 0x01-state/0-counter/src/components/Counter.jsx
+++ 0x01-state/0-counter/src/components/Counter.jsx
@@ -2,9 +2,18 @@ import PropTypes from 'prop-types';
 
 class Counter extends React.Component {
   render() {
+    return (
     <div>
-      
+      <h1>{this.props.value}</h1>
     </div>
-
+    );
   }
 }
+
+Counter.defaultProps = {
+  value: 0
+};
+
+Counter.propTypes = {
+  PropTypes.number.isRequired
+};

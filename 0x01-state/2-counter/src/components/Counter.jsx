2ecb10e49821995d00f6dfba8bea3982744db865
kris <krisbredemeier@gmail.com>
2017-10-15 14:06:26 -0700

    iniates files from previous task

--- 0x01-state/2-counter/src/components/Counter.jsx
+++ 0x01-state/2-counter/src/components/Counter.jsx
@@ -5,14 +5,14 @@ export default class Counter extends React.Component {
   constructor(props) {
     super(props);
     this.state ={
-      x: props.initValue
+      value: props.initValue
     };
   }
 
   render() {
     return (
       <div>
-        {this.props.value ? this.props.value : 0}
+        {this.props.initValue}
       </div>
     );
   }

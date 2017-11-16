b896e67fb80e31f39858f26e588a40f1f284e9ca
kris <krisbredemeier@gmail.com>
2017-11-16 13:34:00 -0800

    counter using redux

---
+++ 0x02-redux/0-counter/src/containers/Counter.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionsCreators } from 'redux';
+
+const Counter = React.createClass({
+  displayName: 'Counter',
+  render () {
+    const { increment, count } = this.props;
+    return(
+      <div>
+        Current counter value: {count}
+        <div><button onClick={(e) => increment()}.Increment</button><div>
+      </div>
+    );
+  }
+});
+
+function mapStateToProps(state) {
+  return {
+    count: state
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionsCreators(actions, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(counter);
---
+++ 0x02-redux/0-counter/src/reducers/Counter.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionsCreators } from 'redux';
+
+const Counter = React.creatClass({
+  displayName: 'Counter',
+  render() {
+    const { increment, count } = this.props;
+    return (
+      <div>
+        Current counter value: {count}
+        <div><button onClick={(e) => increment()}>Increment</button></div>
+      </div>
+    );
+  }
+});
+
+const Increment = 'INCREMENT';
+
+function increment() {
+  return {
+    type: types.Increment
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    count: state
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionsCreators(actions, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);

4b71b25ff74d8634d80877d5950b8e791642b616
kris <krisbredemeier@gmail.com>
2017-11-20 15:46:05 -0800

    creates new counter directory with files inside from previous task

---
+++ 0x02-redux/2-counter/src/containers/Counter.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+class CounterContainer extends React.Component{
+  displayName: 'Counter'
+  render () {
+    const { increment, decrement, count } = this.props;
+    return(
+      <div>
+        Current counter value: {count}
+        <div><button onClick={(e) => increment()}>Increment</button></div>
+        <div><button onClick={(e) => decrement()}>Decrement</button></div>
+      </div>
+    );
+  }
+};
+
+//actions
+
+export function increment() {
+  return {
+    type: "INCREMENT"
+  }
+}
+
+export function decrement() {
+  return {
+    type: "DECREMENT"
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    count: state
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({
+    increment: increment,
+    decrement: decrement
+  },
+    dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);

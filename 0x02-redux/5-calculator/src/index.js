0dc7c4077a77b770ba57ec9d32a60460f6a63188
kris <krisbredemeier@gmail.com>
2017-12-03 21:53:47 -0800

    remove fiels from previous fist four tasks

--- 0x02-redux/5-calculator/src/index.js
+++ 0x02-redux/5-calculator/src/index.js
@@ -3,9 +3,21 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux';
 import App from './components/App.jsx';
+import { combineReducers } from 'redux';
+import number from './reducers/index/jsx'
 
+const allReducers = combineReducers({
+  number: number
+});
+
+const store = createStore(
+  allReducers,
+  applyMiddleware()
+)
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 )

3cc13a4a7e663a2e7cd20fe2010ab299f7a79b2e
kris <krisbredemeier@gmail.com>
2018-02-25 08:50:35 -0800

    match name

--- 0x04-react_router/2-nested/src/components/Curriculum.jsx
+++ 0x04-react_router/2-nested/src/components/Curriculum.jsx
@@ -3,14 +3,14 @@ import { Link, Route } from 'react-router-dom';
 import Year1 from './curriculum/Year1.jsx';
 import Year2 from './curriculum/Year2.jsx';
 
-const Curriculum = ({ match }) => (
+const Curriculum = ({ match }) => { return (
   <div>
     <ul>
-      <li><Link to={`${match.url}/year_1`}>Year 1</Link></li>
-      <li><Link to={`${match.url}/year_2`}>Year 2</Link></li>
+      <li><Link to={`${match.url}/curriculum/year_1`}>Year 1</Link></li>
+      <li><Link to={`${match.url}/curriculum/year_2`}>Year 2</Link></li>
     </ul>
       <Route path={`${match.path}/:name`} render= {({match}) =>( <div> <h3> {match.params.name} </h3></div>)}/>
   </div>
 )
-
+}
 export default Curriculum;

bac6bb00faa33d1fb0ffa49636a942415ded3065
kris <krisbredemeier@gmail.com>
2018-02-25 10:36:14 -0800

    Year react file work

--- 0x04-react_router/4-params_state/src/components/Year.jsx
+++ 0x04-react_router/4-params_state/src/components/Year.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router'
 
+'''
+connect the id of the curriculum, retrun the name and description depending on id
+'''
 class Year extends React.Component {
   render() {
     let curriculum_id = this.props.match.params.year_id;
     let curriculum = this.props.curriculum.filter(function(c) {
       return c.id === curriculum_id;
-    })[0]
+    })
     return (
       <div>
         <h2>{curriculum.name}</h2>

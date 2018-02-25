e87ff8e431d128503abde504177db79eca2dbbd3
kris <krisbredemeier@gmail.com>
2018-02-25 09:15:38 -0800

    params work, but nested still is not correct

--- 0x04-react_router/3-params/src/components/Curriculum.jsx
+++ 0x04-react_router/3-params/src/components/Curriculum.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import { Link, Route } from 'react-router-dom';
 import Year from './Year.jsx';
 
-const Curriculum = ({ match }) => { return (
+const Curriculum = ({ match }) => (
   <div>
     <ul>
-      <li><Link to={`${match.url}/curriculum/year_1`}>Year1</Link></li>
-      <li><Link to={`${match.url}/curriculum/year_2`}>Year2</Link></li>
+      <li><Link to="/curriculum/year_1">Year1</Link></li>
+      <li><Link to="/curriculum/year_2">Year2</Link></li>
+      <li><Link to="/curriculum/year_3">Year3</Link></li>
+      <li><Link to="/curriculum/year_4">Year4</Link></li>
     </ul>
-      <Route path={`${match.path}/curriculum/:name`} render= {({match}) =>( <div> <h3> {match.params.name} </h3></div>)}/>
+      <Route path="/curriculum/:name" component={Year}/>
   </div>
 )
-}
+
 export default Curriculum;

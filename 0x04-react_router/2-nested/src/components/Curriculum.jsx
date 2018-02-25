b19c21ddaa2574f3647927e91e7cea742108adc1
kris <krisbredemeier@gmail.com>
2018-02-24 17:15:19 -0800

    match params for nested llinks

--- 0x04-react_router/2-nested/src/components/Curriculum.jsx
+++ 0x04-react_router/2-nested/src/components/Curriculum.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Link, Route, Switch } from 'react-router-dom';
+import { Link, Route } from 'react-router-dom';
 import Year1 from './curriculum/Year1.jsx';
 import Year2 from './curriculum/Year2.jsx';
 
-const Curriculum = () => (
+const Curriculum = ({ match }) => (
   <div>
     <ul>
-      <li><Link to="/curriculum/year_1">Year 1</Link></li>
-      <li><Link to="/curriculum/year_2">Year 2</Link></li>
+      <li><Link to={`${match.url}/year_1`>Year 1</Link></li>
+      <li><Link to={`${match.url}/year_2`>Year 2</Link></li>
     </ul>
-      <Route path="/curriculum/year_1" component={Year1}/>
-      <Route path="/curriculum/year_2" component={Year2}/>
+      <Route path="{`${match.path}/:name`} render= {({match}) => (
+    <div><h3> {match.params.name} </h3></div>)}/>
   </div>
 )
 

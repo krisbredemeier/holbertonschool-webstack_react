3faa666576f1b51133deb174943e3bb6b3ffe51e
kris <krisbredemeier@gmail.com>
2018-02-25 12:47:28 -0800

    fetch and recieve repos and commit actions

--- 0x04-react_router/5-github/src/actions/Actions.jsx
+++ 0x04-react_router/5-github/src/actions/Actions.jsx
@@ -1,15 +1,24 @@
 import fetch from 'cross-fetch'
 
 export const FETCH_REPOS = 'FETCH_REPOS'
-function fetchRepos() {
+function fetchRepos(repos) {
   return {
     type: FETCHING,
     fetching: true
   }
 }
 
+export const RECIEVE_REPOS = 'RECIEVE_REPOS'
+function recieveRepos(repos) {
+  return {
+    type: RECIEVED_REPOS,
+    fetching: false,
+    repos: repos
+  }
+}
+
 export const FETCH_COMMITS = 'FETCH_COMMITS'
-function rfetchCommits(repo) {
+function rfetchCommits(repos, json) {
   return {
     type: FETCHING,
     fetching: true
@@ -18,16 +27,25 @@ function rfetchCommits(repo) {
   }
 }
 
-export function fetchRepos() {
+export const RECIEVE_COMMITS = 'RECIEVE_COMMITS'
+function recieveCommits(repos, json) {
+  return {
+    type: RECIEVED_COMMITS,
+    fetching: false,
+    commits: commits
+  }
+}
+
+export function fetchRepos(repos) {
    return function (dispatch) {
-      dispatch(fetchRepos())
-      return fetch(`https://api.github.com/users/krisbredemeier/repos`)
+      dispatch(fetchRepos(repos))
+      return fetch(`https://api.github.com/users/krisbredemeier/${repos}.json`)
       .then(
         response => response.json(),
         error => console.log('An error occurred.', error)
      )
      .then(json =>
-       dispatch(fetchRepos())
+       dispatch(recieveRepos(repos, json))
      )
  }
 }
@@ -41,7 +59,7 @@ export function fetchCommits(repo) {
         error => console.log('An error occurred.', error)
      )
      .then(json =>
-       dispatch(fetchCommits())
+       dispatch(recieveCommits())
      )
  }
 }

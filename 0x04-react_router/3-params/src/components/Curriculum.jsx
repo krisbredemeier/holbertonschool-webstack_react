dca48c576cdf8faa0fab5f4d3e12a60be478e18c
kris <krisbredemeier@gmail.com>
2018-02-25 09:00:54 -0800

    nested with params

---
+++ 0x04-react_router/3-params/src/components/Curriculum.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link, Route } from 'react-router-dom';
+import Year1 from './curriculum/Year1.jsx';
+import Year2 from './curriculum/Year2.jsx';
+
+const Curriculum = ({ match }) => { return (
+  <div>
+    <ul>
+      <li><Link to={`${match.url}/year_1`}>Year1</Link></li>
+      <li><Link to={`${match.url}/year_2`}>Year2</Link></li>
+    </ul>
+      <Route path={`${match.path}/:name`} render= {({match}) =>( <div> <h3> {match.params.name} </h3></div>)}/>
+  </div>
+)
+}
+export default Curriculum;
---
+++ 0x04-react_router/3-params/src/components/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
+import Home from './Home.jsx';
+import Holberton from './Holberton.jsx';
+import School from './School.jsx';
+import Curriculum from './Curriculum.jsx';
+
+const Header = () => (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/holberton">Holberton</Link></li>
+        <li><Link to="/school">School</Link></li>
+	      <li><Link to="/curriculum">Curriculum</Link></li>
+      </ul>
+      <switch>
+      	<Route exact path="/" component={Home}/>
+      	<Route path="/holberton" component={Holberton}/>
+      	<Route path="/school" component={School}/>
+        <Route pah="/curriculum" component={Curriculum}/>
+      </switch>
+    </div>
+  </Router>
+)
+export default Header;
